8072e9b60fa12ceb480f6bb49cf66a2a4c4cd2ec
Robyn Sanders <reddie@smartchat.net.au>
2021-10-16 22:50:35 +1100

    Completed assignmnt

---
+++ index.js
@@ -0,0 +1,103 @@
+function add() {
+    return 1 + 80;
+   
+}
+function subtract() {
+    return 60 - 40;
+}
+function multiply() {
+   return 2 * 3.4;
+}
+    
+function divide() {
+    return 2 * 3.4;
+}
+function add(a, b) {
+    return (a + b);
+}
+result = add(3, 90);
+console.log(add(3, 90));
+
+function subtract(a,b) {
+    return (a - b);
+}
+result=subtract(50,30)
+console.log(subtract(50, 30));
+
+function multiply(a,b) {
+    return (a * b);
+}
+result = multiply(12, 36);
+console.log(multiply(12, 36));
+
+function divide(a,b) {
+    return a / b;
+}
+result = divide(144, 8);
+console.log(multiply(144, 8));
+
+
+
+ //let a = 1
+ //a//++
+ //console.log(a++);
+
+ 
+ function increment(n) {
+   n++;
+   console.log(n);
+   return n;
+ }
+ 
+ 
+ function decrement(n) {
+   n--;
+   console.log(n)
+   return n;
+ }
+
+ 
+//function makeInt(a) {//
+   //console.log (parseInt ("42", 10));//
+      //return parseInt ("42", 10);//
+//}//
+
+/*function makeInt(b) {
+    console.log (parseInt ("2.2222", 10));
+    return parseInt ("2.2222", 10);
+}*/
+
+/*function makeInt(c) {
+        let c = parseInt; {
+        parseInt ("He is happy", 10);
+        console.log (parseInt ("He is happy", 10));
+        return parseInt ("He is happy", 10);
+}}
+return makeint(c);*/
+
+function makeInt (n) {
+    return parseInt (n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat (n, 10);
+}
+ 
+  
+
+
+  
+
+  
+
+  
+
+
+
+
+ 
+ 
+  
+
+
+
